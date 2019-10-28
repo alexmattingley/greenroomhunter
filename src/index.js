aaf6492cd4d503aff7e7e1c99aa9fa106ce7e8b8
Alex Mattingley <alexmattingley@gmail.com>
2019-10-28 15:58:26 -0700

    using global style with styled-components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Normalize } from 'styled-normalize';
+import { createGlobalStyle } from 'styled-components'
+import { AppContainer } from './components/Container/index.styled.js';
 import Home from './pages/home.jsx';
 import About from './pages/about.jsx';
 import Contact from './pages/contact.jsx';
@@ -8,16 +11,31 @@ import Location from './pages/location.jsx';
 import NotFound from './pages/notFound.jsx';
 import * as serviceWorker from './serviceWorker';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Montserrat', sans-serif;
+  }
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Roboto', sans-serif;
+  }
+`;
+
 const routing = (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/location/:location" component={Location} />
-      <Route component={NotFound} />
-    </Switch>
-  </Router>
+  <>
+    <GlobalStyle />
+    <Normalize />
+    <AppContainer>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/location/:location" component={Location} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    </AppContainer>
+  </>
 );
 
 ReactDOM.render(routing, document.getElementById('root'));
