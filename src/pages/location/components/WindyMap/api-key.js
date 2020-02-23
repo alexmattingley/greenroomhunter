286d3f33848bc8abdd53d2b44dfec8dccf834fe2
Mattingley <alex_matingley@intuit.com>
2020-02-23 10:27:50 -0800

    exposing key for deploy

--- src/pages/location/components/WindyMap/api-key.js
+++ src/pages/location/components/WindyMap/api-key.js
@@ -1,4 +1,4 @@
 // Google windy api key, or generate new key.
 // Current working URL https://api4.windy.com/api-key
-const key = 'YOUR_KEY_HERE';
+const key = 'rRee3fNI1vLlm34f88k5SJSRAnFm92pZ';
 export default key;
