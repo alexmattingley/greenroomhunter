9b5a3684f6da4c0283052e7962cdabf71384025c
Alex <alexmattingley@gmail.com>
2021-07-26 16:44:08 -0700

    changing timezone for tides to handle daylight savings time

--- data/api-data/noaa/tides/fetch-tide-data.js
+++ data/api-data/noaa/tides/fetch-tide-data.js
@@ -1,4 +1,4 @@
-// https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date=20210617&end_date=20210618&product=predictions&station=9410170&datum=MLLW&units=english&time_zone=lst&application=Web_Services&format=json
+// https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date=20210617&end_date=20210618&product=predictions&station=9410170&datum=MLLW&units=english&time_zone=lst_ldt&application=Web_Services&format=json
 import fetch from 'node-fetch';
 import { DateTime } from 'luxon';
 
@@ -6,7 +6,7 @@ const fetchTideData = async (stationId) => {
   const beginDate = DateTime.now().toISODate().split('-').join('');
   const endDate = DateTime.now().plus({days: 1}).toISODate().split('-').join('');
   try {
-    const tideURL = `https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date=${beginDate}&end_date=${endDate}&product=predictions&station=9410170&datum=MLLW&units=english&time_zone=lst&application=Web_Services&format=json`;
+    const tideURL = `https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date=${beginDate}&end_date=${endDate}&product=predictions&station=9410170&datum=MLLW&units=english&time_zone=lst_ldt&application=Web_Services&format=json`;
     const response = await fetch(tideURL);
     const json = await response.json();
     // TODO: Set up better error handling here, should never reach the next line if response is an error
