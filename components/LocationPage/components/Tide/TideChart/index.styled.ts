2512fb67acc6a2f610ef618e44c7c41d84cb0894
Alex Mattingley <alexmattingley@lucias-air.attlocal.net>
2026-09-17 16:29:33 -0700

    vertical scroll issue

--- components/LocationPage/components/Tide/TideChart/index.styled.ts
+++ components/LocationPage/components/Tide/TideChart/index.styled.ts
@@ -5,6 +5,13 @@ export const TideChartWrapper = styled.div`
   position: relative;
   width: 100%;
   height: 280px;
+  /* While a finger is scrubbing the chart, drive the chart instead of
+     scrolling the page vertically. */
+  touch-action: none;
+
+  canvas {
+    touch-action: none;
+  }
 
   @media only screen and (min-width: ${breakpts.sm}) {
     height: 360px;
